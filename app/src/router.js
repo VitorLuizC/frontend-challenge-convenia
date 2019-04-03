8bf2bbd9ac4e5804a5838e92732641e0ce312880
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-04-03 04:27:22 -0300

    Create Tables view with simple filter and link to Table

--- app/src/router.js
+++ app/src/router.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Tables from '@/views/Tables.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      name: 'Tables',
+      path: '/',
+      component: Tables
+    }
+  ]
 })
