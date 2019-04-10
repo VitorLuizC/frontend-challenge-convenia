dc8e86ea96ac07f2730a1465ec8b5fb023ad8c89
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-04-10 03:08:16 -0300

    Improve base CSS attributes and move them to a module

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,14 +2,4 @@
   <router-view />
 </template>
 
-<style lang="scss">
-body {
-  font-family: 'Heebo', sans-serif;
-  color: #3a416f;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-</style>
+<style lang="scss" src="@/assets/sass/base.scss" />
