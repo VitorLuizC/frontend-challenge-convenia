1243da4ec0b3f3401feaec69bb2aa61699d68022
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-04-11 05:30:52 -0300

    Create VButton component and style calc results

---
+++ app/src/components/VButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <component
+    :is="to ? 'router-link' : 'button'"
+    :to="to"
+    :type="to ? undefined : 'button'"
+    class="v-button"
+  >
+    <span class="text">
+      <slot />
+    </span>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    to: [String, Object]
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/assets/sass/helpers.scss';
+
+.v-button {
+  @include rect(256px, 48px);
+  display: flex;
+  align-items: center;
+  border-radius: 48px;
+  justify-content: center;
+  background-color: #6c5ce7;
+  background-image: linear-gradient(30deg, transparent, #a29bfe);
+  transition: transform 0.8s ease-out;
+  box-shadow: 0 4px 11px 0 rgba(37, 44, 97, 0.15),
+    0 1px 3px 0 rgba(93, 100, 148, 0.2);
+  cursor: pointer;
+
+  & > .text {
+    color: #ffffff;
+    font-weight: 700;
+    font-size: em(14px);
+    text-transform: uppercase;
+    transition: transform 0.8s ease-out;
+  }
+
+  &:active {
+    transition: transform 0.05s ease-in;
+    transform: scale(1.1);
+
+    & > .text {
+      transition: transform 0.05s ease-in;
+      transform: scale(0.9);
+    }
+  }
+}
+</style>
--- app/src/views/Bill.vue
+++ app/src/views/Bill.vue
@@ -14,25 +14,38 @@
 
     <v-line />
 
-    Total (à pagar): {{ unpaidTotal }}<br />
-    Total (pago): {{ paidTotal }}<br />
-    Total: {{ total }}<br />
+    <div class="calc">
+      <p class="result">
+        <span class="name">Valor à pagar</span>
+        <span class="value">{{ toBRL(unpaidTotal) }}</span>
+      </p>
+      <p class="result">
+        <span class="name">Valor pago</span>
+        <span class="value">{{ toBRL(paidTotal) }}</span>
+      </p>
+      <p class="result">
+        <span class="name">Total</span>
+        <span class="value">{{ toBRL(total) }}</span>
+      </p>
+    </div>
 
     <v-line />
 
-    <router-link :to="'/payment/' + id">Pagar</router-link>
+    <v-button class="button" :to="'/payment/' + id">Pagar</v-button>
   </main>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import VLine from '@/components/VLine.vue'
+import VButton from '@/components/VButton.vue'
 import VDetails from '@/components/VDetails.vue'
 import OrderList from '@/components/Order/OrderList.vue'
 import PaymentList from '@/components/Payment/PaymentList.vue'
+import { formatToBRL } from 'brazilian-values'
 
 export default {
-  components: { OrderList, PaymentList, VLine, VDetails },
+  components: { OrderList, PaymentList, VLine, VButton, VDetails },
   props: {
     id: {
       type: String,
@@ -45,6 +58,9 @@ export default {
     paidTotal: 'PAID_TOTAL',
     unpaidTotal: 'UNPAID_TOTAL'
   }),
+  methods: {
+    toBRL: formatToBRL
+  },
   watch: {
     id: {
       immediate: true,
@@ -60,6 +76,32 @@ export default {
 .bill-view {
   @media screen and (min-width: 768px) {
     width: 768px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .calc {
+    box-sizing: border-box;
+    display: block;
+    padding: 0 12px;
+
+    & > .result {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      & > .name {
+        font-size: em(18px);
+      }
+
+      & > .value {
+        font-size: em(18px);
+        font-weight: 500;
+      }
+    }
+  }
+
+  & > .button {
     margin-left: auto;
     margin-right: auto;
   }
